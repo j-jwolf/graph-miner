6be44bd7ff4f8ab2a5bf3541a51b8bcaf076af76
j-jwolf <jjwolf@noctrl.edu>
2022-10-17 13:13:23 -0500

    overhaul of GA and main.py

--- main.py
+++ main.py
@@ -1,115 +1,257 @@
-import os, sys, igraph
+import os, sys, igraph, random
 from utils import *
 
-vars = readJSON("vars.json") # global variables --> easier to change a json file than to dig through code. contains information about genetic algorithm specifically
-
-class Entity:
-	def __init__(self, c): self.__chromosome = c
-	def setChromosome(self, c): self.__chromosome = c # chromosome setter
-	def getChromosome(self): return self.__chromosome # chromosome getter
-	def getFitness(self, t):
-		# gets the fitness of current entity --> how close am i to the target?
-		fitness = 0
-		for a, b in zip(self.__chromosome, t):
-			if(a != b): fitness += 1
-		return fitness
-	def mutate(self, g): return random.choice(g) # get a mutation (random new char from gene pool)
-	def newGenome(self, t):
-		# gets a new genome sequence
-		g = list()
-		for i in range(len(t)): g.append(self.mutate())
-		return g
-	def nextGen(self, other):
-		# gets child of this and other
-		global vars
-		c = list()
-		for a, b in zip(self.chromosome, other.chromosome):
-			weight = random.random()
-			if(weight < vars["LOWER_BOUND"]): c.append(a)
-			elif(weight < vars["UPPER_BOUND"]): c.append(b)
-			else: c.append(self.mutate())
-		return Entity(c)
-
-
-def keyExit(): sys.exit(-2) # quick exit for keyboard interrupt
-def line(): return f"\n{'='*50}\n" # for formatting
-def vertexFound(g, v):
-	try: g.vs.find(id = v)
-	except KeyboardInterrupt: kexit()
-	except Exception: return False
-	return True
-def getConnecting(nodes):
-	nodes = None
-	userInput = ""
-	while(userInput != "e"):
+class Individual:
+	POP_SIZE = 100
+	@classmethod
+	def mutate(cls, edges, genome): return random.choice(edges)
+	def isValid(self):
+		global allPaths
+		nodes = set()
+		for tup in self.__chromosome:
+			nodes.add(tup[0])
+			nodes.add(tup[1])
+		nodes = list(nodes)
+		nodes.sort()
+		return nodes in allPaths or Individual.isSubset(nodes, allPaths)
+	@classmethod
+	def isSubset(cls, nodes, paths):
+		count = 0
+		found = False
+		a = set(nodes)
+		while(count < len(paths) and not found):
+			b = set(paths[count])
+			if(b.issubset(a)): found = True
+			count += 1
+		if(not found): return found
 		...
-	return nodes
+		return found
+	def __init__(self, c, source, target):
+		self.__chromosome = c
+		self.__source = source
+		self.__target = target
+	def setChromosome(self, c): self.__chromosome = c
+	def setSource(self, source): self.__source = source
+	def setTarget(self, target): self.__target = target
+	def getChromosome(self): return self.__chromosome
+	def getSource(self): return self.__source
+	def getTarget(self): return self.__target
+	def fitness(self):
+		if(self.isValid()): return len(self.__chromosome)
+		return sys.maxsize
+	def crossover(self, other):
+		global TARGET
+		probs = [0.45, 0.9]
+		child = list()
+		#for i in range(random.randint(TARGET-3, TARGET)):
+		#n = random.randint(int(TARGET/3), TARGET)
+		#TARGET = n
+		n = random.randint(1, TARGET)
+		for i in range(n):
+			weight = random.random()
+			if(weight < probs[0]): child.append(random.choice(self.__chromosome))
+			elif(weight < probs[1]): child.append(random.choice(other.__chromosome))
+			else: child.append(Individual.mutate(edges, child))
+		return Individual(child, self.__source, self.__target)
 
-try:
-	graph = igraph.Graph(directed = False)
+class Node:
+	def __init__(self, data, next):
+		self.__data = data
+		self.__next = next
+	def setData(self, data): self.__data = data
+	def setNext(self, next): self.__next = next
+	def getData(self): return self.__data
+	def getNext(self): return self.__next
 
-	userInput = ""
-	while(userInput != "e" and not os.path.isfile(userInput)):
-		userInput = input("Enter input file name or 'e' to exit: ")
-		if(userInput != "e" and not os.path.isfile(userInput)): print(f"{userInput} is not a valid file")
+class Stack:
+	def __init__(self): self.__head = None
+	def push(self, obj):
+		if self.__headnext is None: next = None
+		else: next = self.__head
+		self.__head = Node(obj, next)
+	def pop(self):
+		if self.__head is None: return None
+		data = self.__head.getData()
+		self.__head = self.__head.getNext()
+		return data
+	def peek(self):
+		if self.__head is None: return None
+		return self.__head.getData()
 
-	if(userInput == "e"): sys.exit(1)
-	inputFile = userInput
+class Writer:
+	def __init__(self): self.__buffer = ""
+	def append(self, s):
+		if(type(s) != type("")):
+			try: self.__buffer += str(s)
+			except KeyboardInterrupt: sys.exit()
+			except Exception: print(f"{s} is cannot be converted to string")
+		else: self.__buffer += s
+	def dump(self, fn):
+		try:
+			if(os.path.isfile(fn)): mode = "a"
+			else: mode = "a"
+			with open(fn, mode) as file: file.write(self.__buffer)
+			self.__buffer = ""
+			return True
+		except KeyboardInterrupt: sys.exit()
+		except Exception as e: print(e)
+		return False
 
-	rawEdges = readFile(inputFile, True)
-	#subset = readFile(subsetFile)
-	edges = list()
+def getRandom(p, weight):
+	# p: population
+	valid = False
+	entity = None
+	while(not valid):
+		entity = random.choice(p[:weight])
+		if(isValidPath(entity.getChromosome(), entity.getSource(), entity.getTarget())): valid = True
+	return entity
+def newGenome(edges):
+	global TARGET
+	genome = list()
 	count = 0
-	vCount = 0
-	verts = set()
-
-	# read in existing edges, gets number of vertices from this data using a set
-	for line in rawEdges:
-		if(count >= 2):
-			tok = line.split(" ")
-			tup = (int(tok[0]), int(tok[1]))
-			edges.append(tup)
-			verts.add(int(tok[0]))
-			verts.add(int(tok[1]))
-		else: pass
+	while(count < TARGET):
+		genome.append(Individual.mutate(edges, genome))
 		count += 1
+	return genome
+def isValidPath(path, source, target):
+	current = a
+	#visited = set()
+	count = 0
+	#while(current != target and len(visited) < len(path) and count < len(path)):
+	while(current != target and count < len(path)):
+		for edge in path:
+			#if(current in edge and not edge in visited):
+			if(current in edge):
+				if(current == edge[0]): current = edge[1]
+				else: current = edge[0]
+				#visited.add(edge)
+		count += 1
+	return current == target
+def getMin(p):
+	m = sys.maxsize
+	index = 0
+	count = 0
+	for e in p:
+		if(e.fitness() < m):
+			m = e.fitness()
+			index = count
+		count += 1
+	return p[index]
+def getNodes(path):
+	nodes = set()
+	for tup in path:
+		nodes.add(tup[0])
+		nodes.add(tup[1])
+	return list(nodes)
+def checkSubset(target, paths):
+	count = 0
+	found = False
+	a = set(target)
+	while(count < len(paths) and not found):
+		b = set(paths[count])
+		if(b.issubset(a)): found = True
+		count += 1
+	return found
+def checkPop(graph, pop):
+	allPaths = graph.get_all_simple_paths(pop[0].getSource(), pop[0].getTarget())
+	count = 0
+	found = False
+	writer = Writer()
+	while(count < len(pop) and not found):
+		# path = all nodes in path
+		path = getNodes(pop[count].getChromosome())
+		path.sort()
+		if path in allPaths: found = True
+		else: found = checkSubset(path, allPaths)
+		writer.append(f"\n{path} in allPaths?: {found}")
+		count += 1
+	writer.dump("output.txt")
+	return found
+def writeAllPaths(paths):
+	writer = Writer()
+	writer.append("ALL PATHS:")
+	for path in paths: writer.append(f"\n{str(path)}")
+	writer.dump("output.txt")
+	return
 
-	# prints vertices in this graph
-	print(f"printing {len(verts)} vertices")
-	for vert in verts: print(vert)
+inputFile = "input.txt"
+rawEdges = readFile(inputFile, True)
+edges = list()
+verts = set()
+count = 0
+vcount = 0
+for line in rawEdges:
+	if(count >= 2):
+		tok = line.split(" ")
+		tup = (int(tok[0]), int(tok[1]))
+		edges.append(tup)
+		verts.add(int(tok[0]))
+		verts.add(int(tok[1]))
+	else: pass
+	count += 1
 
-	# prints edges in this graph
-	print(f"\n\nprinting {len(edges)} edges")
-	for edge in edges: print(edge)
+graph = igraph.Graph(directed = False)
+graph.add_vertices(len(verts))
+for _i in range(len(verts)):
+	graph.vs[_i]["id"] = _i
+	graph.vs[_i]["label"] = str(_i)
+graph.add_edges(edges)
 
-	# adding vertices to graph and labeling them
-	graph.add_vertices(len(verts))
-	for i in range(len(graph.vs)):
-		graph.vs[i]["id"] = i
-		graph.vs[i]["label"] = str(i)
+TARGET = len(edges)
+START = len(edges)
+CARRYOVER = 10
+CROSSOVER = 50
+a = 0
+b = 14
+cutoff = 50
 
-	graph.add_edges(edges)
-	try:
-		#print(f"10 in graph?: {10 in graph.vs}")
-		#print(f"TEST --> id=10: {graph.vs.find(id=10)}\tid=20: {graph.vs.find(id=20)}")
-		print(f"10 in graph?: {vertexFound(graph, 10)}\n20 in graph?: {vertexFound(graph, 20)}")
-	except KeyboardInterrupt: kexit()
-	except Exception as e: print(e)
+generation = 1
+pop = list()
+for _i in range(Individual.POP_SIZE): pop.append(Individual(newGenome(edges), a, b))
 
-	if(not os.path.isfile("test.png")):
-		# delete me -- this is for viewing that the graph information is correct
-		vs = {}
-		output = "test.png"
-		vs["bbox"] = (400, 400)
-		vs["margin"] = 27
-		vs["vertex_color"] = "white"
-		vs["vertex_size"] = 45
-		vs["vertex_label_size"] = 22
-		vs["edge_curved"] = False
-		layout = graph.layout()
-		vs["layout"] = layout
-		igraph.plot(graph, output, **vs)
-except KeyboardInterrupt: kexit()
-except Exception as e:
-	print(e)
-	sys.exit(-1)
+#writeAllPaths(graph.get_all_simple_paths(a, b))
+allPaths = graph.get_all_simple_paths(a, b)
+
+while(generation < cutoff):
+	#pop = sorted(pop, key = lambda entity : entity.fitness())
+	pop.sort(key = lambda entity : entity.fitness())
+	#if(pop[0].fitness() != sys.maxsize):
+		#if(TARGET > 1): TARGET 0-1
+	"""
+	if(isValidPath(pop[0].getChromosome(), pop[0].getSource(), pop[0].getTarget())):
+		print("pop[0] is valid")
+		if(TARGET > 1): TARGET -= 1
+	"""
+	next = list()
+	s = int((CARRYOVER*Individual.POP_SIZE)/100)
+	next.extend(pop[:s])
+	s = int((CROSSOVER*Individual.POP_SIZE)/100)
+	for _i in range(Individual.POP_SIZE-CARRYOVER):
+		valid = False
+		while(not valid):
+			parents = [
+				random.choice(pop[:CROSSOVER]),
+				random.choice(pop[:CROSSOVER])
+			]
+			if(parents[0].fitness() != 0 and parents[1].fitness() != 0): valid = True
+		child = parents[0].crossover(parents[1])
+		next.append(child)
+	pop = next
+	#m = getMin(pop)
+	#if(m.fitness() < TARGET): TARGET = m.fitness()
+	#pop.sort(key = lambda entity : entity.fitness())
+	#if(isValidPath(pop[0].getChromosome(), pop[0].getSource(), pop[0].getTarget())):
+	"""
+	if(checkPop(graph, pop)):
+		if(TARGET > 1): TARGET -= 1
+	else:
+		if(TARGET < START): TARGET += 1
+	"""
+	print(f"End of generation {generation}")
+	generation += 1
+
+pop.sort(key = lambda entity : entity.fitness())
+
+#print(f"Fittest individual: {pop[0].fitness()}\n\tIts chromosome: {pop[0].getChromosome()}")
+print(f"Fittest individual: {pop[0].fitness()}\nIts edges:")
+for edge in pop[0].getChromosome(): print(f"\n{edge}")
